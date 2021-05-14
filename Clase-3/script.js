9bfe7a9d4a16ec1410d683937d83880a56cc2f6c
Javier Castaños <javocz.10@gmail.com>
2021-05-13 21:09:16 -0300

    Desafio Clase 3

--- Clase-3/script.js
+++ Clase-3/script.js
@@ -1,12 +1,12 @@
-let guessNumber = 738;
+let guessNumber = 8;
 let number = 0;
 let attempts = 0;
 
 do {
-  number = parseInt(prompt("Crees poder adivinar mi numero MENOR A 1000 favorito?"));
+  number = parseInt(prompt("Del 1 al 100, cuál es mi numero favorito?"));
   attempts = attempts + 1;
-} while ((number < 1000) && !(guessNumber==number)){
-  if (number > 1000) {
+} while ((number < 100) && !(guessNumber==number)){
+  if (number > 100) {
     alert("Ingresaste un valor mayor a 1000. Refresca la página para empezar de vuelta");
   } else {
     alert("Muy bien!!! Te tomó " + attempts + " intentos, pero lo adivinaste.");
